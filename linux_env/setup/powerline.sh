f1a27ae9047d2c70c96baf5ac1c08169ab680c00
Rafael Zuquim <rzuquim@inoa.com.br>
2022-02-26 17:03:36 -0300

    fix(linux): powerline setup (including fonts)

--- linux_env/setup/powerline.sh
+++ linux_env/setup/powerline.sh
@@ -1,8 +1,10 @@
+sudo apt-get install fonts-powerline
+
 pip install --user powerline-status
 pip install powerline-gitstatus
 
-mkdir ~/.config/powerline/colorschemes
+mkdir -p ~/.config/powerline/colorschemes
 cp ./linux_env/setup/powerline.colorschemes.json ~/.config/powerline/colorschemes/default.json
 
-mkdir ~/.config/powerline/themes/shell
+mkdir -p ~/.config/powerline/themes/shell
 cp ./linux_env/setup/powerline.git-shell.json ~/.config/powerline/themes/shell/default.json
