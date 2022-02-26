c96d3bcb40c2edf89d9f2ea19785add9393bcd27
Rafael Zuquim <rzuquim@inoa.com.br>
2022-02-26 13:20:38 -0300

    feat(dev): dotnet auto-complete

--- windows_env/profile/devenv.ps1
+++ windows_env/profile/devenv.ps1
@@ -1,3 +1,11 @@
 $env:HOST_IP = (Test-Connection -ComputerName (hostname) -Count 1).IPV4Address.IPAddressToString;
 
 Set-Alias -Name opencover -Value OpenCover.Console.exe
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
