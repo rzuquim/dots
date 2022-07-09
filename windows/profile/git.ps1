86703a41961b55a9dba8af5c473d910fdd1fee08
Rafael Zuquim <rzuquim@inoa.com.br>
2022-07-08 21:52:25 -0300

    refactor(win): review alias to call 'git gui' (necessary due to oh-my-posh bug)

--- windows/profile/git.ps1
+++ windows/profile/git.ps1
@@ -17,3 +17,13 @@ function on-github($path = '/tree/@branch') {
 }
 
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
+# workaround on git gui imcompatibility with oh-my-posh
+function review() {
+  $job = Start-Job { cd $args[0]; git gui } -ArgumentList $PWD
+  Register-ObjectEvent -InputObject $job -EventName StateChanged -Action {
+      Unregister-Event $EventSubscriber.SourceIdentifier
+      Remove-Job $EventSubscriber.SourceIdentifier
+      Remove-Job -Id $EventSubscriber.SourceObject.Id
+  } | Out-Null
+}
