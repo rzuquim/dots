6bf029689e10b372b4640949efaf2549de9043c2
Rafael Zuquim <rzuquim@inoa.com.br>
2023-08-21 11:42:58 -0300

    feat(ps): showing every profile script startup time

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -3,6 +3,9 @@ $profileScripts = (Split-Path $PROFILE) + "\profile"
 
 Get-ChildItem ("$profileScripts/*.ps1") | ForEach-Object {
     $profileScript = (Join-Path $profileScripts $_.Name)
-    . $profileScript
+    $elapsed = (Measure-Command {
+        . $profileScript 
+    }).TotalMilliseconds
+    Write-Host $_.Name " loaded in " $elapsed "ms"
 } | Out-Null
 
