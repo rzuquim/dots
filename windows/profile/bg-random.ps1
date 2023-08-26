f5a556d262b5cf8d74f8b9d4affed38313b0bab5
Rafael Zuquim <rzuquim@inoa.com.br>
2023-08-26 09:46:25 -0300

    feat(win): bg-random updates lockscreen

--- windows/profile/bg-random.ps1
+++ windows/profile/bg-random.ps1
@@ -1,20 +1,21 @@
 
 function bg-random {
   # Specify the path to the folder containing your background images
-  $ImagePath = "$env:HOMEPATH/.dots/common/wallpapers"
+  $ImagePath = "$env:HOMEPATH\.dots\common\wallpapers"
 
-# Get all image files from the specified folder
+  # Get all image files from the specified folder
   $ImageFiles = Get-ChildItem -Path $ImagePath -Filter "*.jpg" -File
 
-# If no images are found, exit the script
+  # If no images are found, exit the script
   if ($ImageFiles.Count -eq 0) {
       Write-Host "No image files found in the specified folder."
       exit
   }
 
-# Pick a random image from the array
-  $RandomImage = Get-Random -InputObject $ImageFiles
+  # Pick a random image from the array
+  $RandomImage = (Get-Random -InputObject $ImageFiles).FullName
 
+  # wallpaper
   $SetWallpaperSrc = @"
 using System.Runtime.InteropServices;
 
@@ -22,7 +23,6 @@ public class Wallpaper {
   public const int SetDesktopWallpaper = 20;
   public const int UpdateIniFile = 0x01;
   public const int SendWinIniChange = 0x02;
-
   [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
   private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
   public static void SetWallpaper(string path) {
@@ -31,7 +31,22 @@ public class Wallpaper {
 }
 "@
   Add-Type -TypeDefinition $SetWallpaperSrc
+  [Wallpaper]::SetWallpaper($RandomImage)
 
-  [Wallpaper]::SetWallpaper($RandomImage.FullName)
+  # change lock screen
+  $RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+
+  if (!(Test-Path $RegKeyPath)) {
+    New-Item -Path $RegKeyPath -Force | Out-Null
+  }
+  # Creating registry entries for Lock Screen
+  $LockScreenStatus = "LockScreenImageStatus"
+  $LockScreenPath = "LockScreenImagePath"
+  $LockScreenUrl = "LockScreenImageUrl"
+  $StatusValue = "1"
+
+  New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
+  New-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -Value $RandomImage -PropertyType STRING -Force | Out-Null
+  New-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -Value $RandomImage -PropertyType STRING -Force | Out-Null
 }
 
