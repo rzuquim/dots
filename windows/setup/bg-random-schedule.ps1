0d782f585b3fbc46a878e93e1ca64da93644d163
Rafael Zuquim <rzuquim@inoa.com.br>
2023-09-10 17:38:35 -0700

    feat(win): bg-random changing login screen bg

--- windows/setup/bg-random-schedule.ps1
+++ windows/setup/bg-random-schedule.ps1
@@ -1,5 +1,5 @@
 $ScriptFile = "$env:HOMEPATH\.dots\windows\setup\bg-random-change.ps1"
-$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File $ScriptFile"
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -File $ScriptFile"
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
 $Settings = New-ScheduledTaskSettingsSet
 Register-ScheduledTask -TaskName "SetRandomBackground" -Action $Action -Trigger $Trigger -Settings $Settings
