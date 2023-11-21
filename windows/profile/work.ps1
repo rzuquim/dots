b8e111227c25b13ccaf224138cf860e7f0f7e573
Rafael Zuquim <rzuquim@inoa.com.br>
2023-11-21 08:20:02 -0300

    feat(win): work command filling the fzf query with $args

--- windows/profile/work.ps1
+++ windows/profile/work.ps1
@@ -1,5 +1,11 @@
 
 function work() {
+  if ($args.Count -eq 0) {
+    $query = ""
+  } else {
+    $query = "-q $args"
+  }
+
   fd `
     --type directory `
     --unrestricted `
@@ -9,7 +15,7 @@ function work() {
     --prune ^.git$ D:\dev | `
   split-path -Parent | Select-Object { "[" + [System.IO.Path]::GetFileName($_) + "] " + $_ } | `
   Format-Table -HideTableHeaders ` |
-  fzf | `
+  fzf -1 $query | `
   awk '{ print $2 }' |
   Set-Location
 }
